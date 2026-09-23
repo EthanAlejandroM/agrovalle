bf3b791f42465db888e1c7af0833676cdf5edb77
Nicolle <nicolmera391@gmail.com>
2026-09-23 14:46:28 -0500

    [feat]:Add Spring Boot application main class

---
+++ src/main/java/com/agrovalle/AgrovalleApplication.java
@@ -0,0 +1,12 @@
+package com.agrovalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrovalleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AgrovalleApplication.class, args);
+  }
+}
