a430d8603e82203e750dc710cee09ab6a6998f6c
Nicolle <nicolmera391@gmail.com>
2026-09-23 16:28:27 -0500

    feat(dataBase):Add database schema migration

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public.usuario
+(
+    id serial NOT NULL,
+    nombre character varying NOT NULL,
+    "ubicacionValle" character varying NOT NULL,
+    documento character varying NOT NULL UNIQUE,
+    rol character varying NOT NULL,
+    "fechaRegistro" date NOT NULL,
+    PRIMARY KEY (id)
+);
